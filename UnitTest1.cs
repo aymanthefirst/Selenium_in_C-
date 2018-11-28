92b0c61608640fa2100b0bc64e471e13d9e60a53
DESKTOP-7BOO3DJ\Lenovo <AHarake@spartaglobal.com>
2018-11-28 12:17:34 +0000

    done

---
+++ UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/maps/");
+            driver.Manage().Window.Maximize();
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
